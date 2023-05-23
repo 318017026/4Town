945d2bcf02f3f4f769b978cc2030377a66e6ed07
TaniaRmz <88344062+TaniaRmz@users.noreply.github.com>
2023-05-22 19:47:52 -0600

    Add files via upload

--- Administrador.py
+++ Administrador.py
@@ -1,9 +1,9 @@
 class Administrador(Usuario):
     
-    #El formato de búsqueda deber ser de tipo "nombre=Maria, apellido paterno=Rodriguez"
+    #El formato de búsqueda deber ser de tipo "nombre=Maria, apeidoP=Rodriguez"
     def consulta_cliente(busqueda):
         busqueda = busqueda.lower()
-        busqueda = busqueda.split("=")
+        busqueda = busqueda.split(", ")
     
         #Establecer conexion con la base de datos
         db = mysql.connector.connect(
@@ -19,21 +19,10 @@ class Administrador(Usuario):
         consulta = ""
         for elem in busqueda:
             i=0
-            if (i%2 == 0):  
-                if elem == "apellido paterno":
-                    elem = "apeidoP"
-                elif elem == "apellido materno":
-                    elem = "apeidoM"
-                elif elem == "edad":
-                    elem = "DATEDIFF(YEAR, fechaNac, GETDATE())"
-                elif elem == "dirección":
-                    elem = "direccion"
-                elif elem == "número":
-                    elem = "numero"
-
-                consulta = consulta++elem
+            if (i > 0):                
+                consulta = consulta++"AND"++elem
             else:
-                consulta = consulta ++ "AND" ++ elem
+                consulta = elem
             i+=1
            
         #Obtener cliente de la base de datos
