5477bbfd0a41de993400ee2b00e0a5977d81d035
TaniaRmz <88344062+TaniaRmz@users.noreply.github.com>
2023-06-02 14:17:07 -0600

    Delete Software directory

--- Software/alchemy_classes/Vendedor.py
+++
@@ -1,9 +0,0 @@
-from alchemy_classes.__init__ import db
-from alchemy_classes.Usuario import Usuario
-
-class Vendedor(db.Model): # type: ignore
-    __tablename__='Vendedor'
-    id = db.Column("id",db.Integer, db.ForeignKey(Usuario.id), primary_key = True)
-
-    def __init__(self, id):
-        self.id = id
