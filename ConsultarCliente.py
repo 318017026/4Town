4c11c3ece79c1f53c6cf02d0b5fc68ffed7c0980
TaniaRmz <88344062+TaniaRmz@users.noreply.github.com>
2023-06-06 08:38:53 -0600

    Añadir vista general

--- ConsultarCliente.py
+++ ConsultarCliente.py
@@ -2,50 +2,77 @@ from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+    
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'eA20052014'
-app.config['MYSQL_DB'] = 'Templete_DB'
+app.config['MYSQL_PASSWORD'] = 'Pa55word'
+app.config['MYSQL_DB'] = 'ingsoft'#'Templete_DB'
 mysql = MySQL(app)
 
-@app.route('/')
-def Index():
-    return render_template('index.html')
-
+#Muestra las opciones de consulta en la pestaña consultar cliente
 @app.route('/consultar_cliente')
 def consultar_cliente():
-    return render_template('index.html')
+    try:
+        cursor=mysql.connection.cursor()
+        cursor.execute('SELECT (nombre, apellidoP, apellidoM, fechaNacimiento, correo, direccion) FROM (Usuario JOIN Cliente)')
+        result = cursor.fetchall()
+        if result == []:
+            return 'No existen clientes en este momento'
+        else: 
+            return render_template('consultar_cliente.html', results=result)
+    except Exception as e:
+        return 'Lo sentimos, ocurrió un error, intente de nuevo más tarde.  \n Error: '+ str(e)
 
-@app.route('/consultar_cliente_nombre', methods=['POST'])
+        
+#Realiza la consulta del cliente tomando en cuenta el nombre
+@app.route('/consultar_cliente', methods=['POST'])
 def consultar_cliente_nombre():
-    if request.method == 'POST':
-        nombre=request.form['nombre']        
-        cursor=mysql.connection.cursor()
-        cursor.execute('SELECT * FROM (Usuario JOIN Cliente) WHERE (nombre='+nombre+')')
-        print(nombre)
-        return 'Los datos del cliente son los siguientes:'
+    try:
+        if request.method == 'POST':
+            nombre=request.form['nombre']  
+            cursor=mysql.connection.cursor()
+            cursor.execute('SELECT (nombre, apellidoP, apellidoM, fechaNacimiento, correo, direccion) FROM (Usuario JOIN Cliente) WHERE (nombre= %s)',(nombre))
+            result=cursor.fetchall()
+            if result == []:
+                return "No se encontró ningún cliente con los datos ingresados. \n Si cree que se trata de un error revise los datos ingresados e intente de nuevo. "
+            else:
+                return render_template('consultar_cliente.html', results=result)
+    except Exception as e: 
+        print("Lo sentimos, ocurrió un error: ", e)
 
-@app.route('/consultar_cliente_apellidoP', methods=['POST'])
+#Realiza la consulta del cliente tomando en cuenta el apellido paterno
+@app.route('/consultar_cliente', methods=['POST'])
 def consultar_cliente_apellidoP():
-    if request.method == 'POST':
-        apellidoP=request.form['apellidoP']        
-        cursor=mysql.connection.cursor()
-        cursor.execute('SELECT * FROM Usuario WHERE (apellidoP==%s)',(apellidoP))
-        mysql.connection.commit()
-        return 'Los datos del cliente son los siguientes:'
+    try:
+        if request.method == 'POST':
+            apellidoP=request.form['apellidoP']        
+            cursor=mysql.connection.cursor()
+            cursor.execute('SELECT (nombre, apellidoP, apellidoM, fechaNacimiento, correo, direccion) FROM (Usuario JOIN Cliente) WHERE (apellidoP=%s)',(apellidoP))
+            result=cursor.fetchall()
+            if result == [] :
+                return "No se encontró ningún cliente con los datos ingresados. \n Si cree que se trata de un error revise los datos ingresados e intente de nuevo. "
+            else:
+                return render_template('consultar_cliente.html', results=result)
+    except TypeError as type_e:
+        return "Lo sentimos, ocurrió un error: ", type_e
+    except Exception as e:
+        return "Lo sentimos, ocurrió un error: ", e
 
-@app.route('/consultar_cliente_edad', methods=['POST'])
+#Realiza la consulta del cliente tomando en cuenta su edad
+@app.route('/consultar_cliente', methods=['POST'])
 def consultar_cliente_edad():
-    if request.method == 'POST':
-        edad=request.form['edad']        
-        cursor=mysql.connection.cursor()
-        cursor.execute('SELECT * FROM Usuario WHERE (edad)')
-        print(edad)
-        return 'Los datos del cliente son los siguientes:'
-    
+    try:
+        if request.method == 'POST':
+            edad=request.form['edad']        
+            cursor=mysql.connection.cursor()
+            cursor.execute('SELECT (nombre, apellidoP, apellidoM, fechaNacimiento, correo, direccion) FROM (Usuario JOIN Cliente) WHERE (DATEDIFF(YEAR, fechaNacimiento, GETDATE())=%s)',(edad))
+            result=cursor.fetchall()
+            if result == []:
+                return "No se encontró ningún cliente con los datos ingresados. \n Si cree que se trata de un error revise los datos ingresados e intente de nuevo. "
+            else:
+                return render_template('consultar_cliente.html', results=result)
+    except Exception as e:
+        print ("Lo sentimos, ocurrió un error:", e)
 
-@app.route('/consultar_reporte_venta')
-def consultar_reporte_venta():
-    return "Consultar reporte de venta"
-if __name__ == '__main__':
-    app.run(port = 80, debug = True)
+app.run(port=80, debug=True)
